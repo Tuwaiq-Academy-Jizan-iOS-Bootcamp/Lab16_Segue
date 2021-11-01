3f2292adc207aa3df9dff5f2b1b88070bb6e8288
laila hakami <laila.hakami.jiz.s@tuwaiq.edu.sa>
2021-11-01 10:32:01 +0300

    layla lab16

---
+++ sugue lab16/sugue lab16/ViewControllerThree.swift
@@ -0,0 +1,12 @@
+//
+//  ViewControllerThree.swift
+//  sugue lab16
+//
+//  Created by layla hakami on 26/03/1443 AH.
+//
+
+import Foundation
+import UIKit
+class ViweControllerThree:UIViewController{
+    
+}
---
+++ sugue lab16/sugue lab16Tests/sugue_lab16Tests.swift
@@ -0,0 +1,33 @@
+//
+//  sugue_lab16Tests.swift
+//  sugue lab16Tests
+//
+//  Created by layla hakami on 26/03/1443 AH.
+//
+
+import XCTest
+@testable import sugue_lab16
+
+class sugue_lab16Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
