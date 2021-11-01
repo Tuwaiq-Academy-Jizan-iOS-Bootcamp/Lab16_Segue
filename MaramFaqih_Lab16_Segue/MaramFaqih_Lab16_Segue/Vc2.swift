c6ba89cd7b46734e91305366621b0cdf4535d77c
maram <maramfaqih92@gmail.com>
2021-11-01 10:25:00 +0300

    Lab 16 : Maram Faqih

---
+++ MaramFaqih_Lab16_Segue/MaramFaqih_Lab16_Segue/Vc2.swift
@@ -0,0 +1,13 @@
+//
+//  Vc2.swift
+//  MaramFaqih_Lab16_Segue
+//
+//  Created by meme f on 26/03/1443 AH.
+//
+
+
+import Foundation
+import UIKit
+class Vc2: UIViewController{
+    
+}
---
+++ MaramFaqih_Lab16_Segue/MaramFaqih_Lab16_Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaramFaqih_Lab16_Segue
+//
+//  Created by meme f on 26/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
