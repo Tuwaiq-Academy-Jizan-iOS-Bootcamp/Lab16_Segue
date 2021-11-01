08a5d9f1bddc7626eadb3e8e341a98d02d13c669
AMALJELI <Aml3mr@hotmail.com>
2021-11-01 12:32:31 +0300

    AMAL lab 16

---
+++ AMAL JELI _10B16 VIEW CONTROLER/AMAL JELI _10B16 VIEW CONTROLER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMAL JELI _10B16 VIEW CONTROLER
+//
+//  Created by Amal Jeli on 26/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AMAL JELI _10B16 VIEW CONTROLER/AMAL JELI _10B16 VIEW CONTROLERTests/AMAL_JELI__10B16_VIEW_CONTROLERTests.swift
@@ -0,0 +1,33 @@
+//
+//  AMAL_JELI__10B16_VIEW_CONTROLERTests.swift
+//  AMAL JELI _10B16 VIEW CONTROLERTests
+//
+//  Created by Amal Jeli on 26/03/1443 AH.
+//
+
+import XCTest
+@testable import AMAL_JELI__10B16_VIEW_CONTROLER
+
+class AMAL_JELI__10B16_VIEW_CONTROLERTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
