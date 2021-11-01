d4f017dc5a238f7279107949edfe6a7a19c3de55
Hanan Somily <hanan.somily.jiz.s@tuwaiq.edu.sa>
2021-11-01 10:32:42 +0300

    HananSomily_Lab16_Segue

---
+++ HananSomily_lab16_Segue/HananSomily_lab16_Segue/ViewControllerThree.swift
@@ -0,0 +1,12 @@
+//
+//  ViewControllerThree.swift
+//  HananSomily_lab16_Segue
+//
+//  Created by Hanan Somily on 01/11/2021.
+//
+
+import Foundation
+import UIKit
+class ViewControllerThree:UIViewController{
+    
+}
---
+++ HananSomily_lab16_Segue/HananSomily_lab16_SegueTests/HananSomily_lab16_SegueTests.swift
@@ -0,0 +1,33 @@
+//
+//  HananSomily_lab16_SegueTests.swift
+//  HananSomily_lab16_SegueTests
+//
+//  Created by Hanan Somily on 01/11/2021.
+//
+
+import XCTest
+@testable import HananSomily_lab16_Segue
+
+class HananSomily_lab16_SegueTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
