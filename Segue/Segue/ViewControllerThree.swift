918413a787120675189218d518f1174a16b3a9f2
Abdulrahman Gahzwni <Abdulrahman.gahzwni.jiz.s@tuwaiq.edu.sa>
2021-11-01 10:37:00 +0300

    A .Gahzwni _ Lab16_Segue

---
+++ Segue/Segue/ViewControllerThree.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerThree.swift
+//  Segue
+//
+//  Created by Abdulrahman Gazwani on 26/03/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerThree : UIViewController {
+    
+}
---
+++ Segue/Segue/ViewControllerTwo.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerTwo.swift
+//  Segue
+//
+//  Created by Abdulrahman Gazwani on 26/03/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    
+}
+
