384470643bf3247cd24597137eecaf5c77c7ab7b
MUSAMASMALI <musa14077@yahoo.com>
2021-11-01 11:17:33 +0300

    musa masmali

---
+++ segues_16/segues 16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segues 16
+//
+//  Created by موسى مسملي on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ segues_16/segues 16/ViewControllerTwo.swift
@@ -0,0 +1,12 @@
+//
+//  ViewControllerTwo.swift
+//  segues 16
+//
+//  Created by موسى مسملي on 01/11/2021.
+//
+
+import Foundation
+import UIKit
+class  ViewControllerTow :UIViewController {
+    
+}
