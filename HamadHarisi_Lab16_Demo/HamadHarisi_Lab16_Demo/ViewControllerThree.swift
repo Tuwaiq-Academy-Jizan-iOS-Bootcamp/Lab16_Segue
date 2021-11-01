881bf44697ce365f36f86ff9913bf329a689a062
hamad <7amad7arisi@gmail.com>
2021-11-01 10:14:50 +0300

    HamadHarisi_Lab16.

---
+++ HamadHarisi_Lab16_Demo/HamadHarisi_Lab16_Demo/ViewControllerThree.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerThree.swift
+//  HamadHarisi_Lab16_Demo
+//
+//  Created by حمد الحريصي on 01/11/2021.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerThree: UIViewController
+{
+    
+}
---
+++ HamadHarisi_Lab16_Demo/HamadHarisi_Lab16_Demo/ViewControllerTwo.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerTwo.swift
+//  HamadHarisi_Lab16_Demo
+//
+//  Created by حمد الحريصي on 01/11/2021.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerTwo: UIViewController
+{
+    
+}
