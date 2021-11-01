1a357515d0b90b9f0e9f53055f8235e89e5d2104
Belal <Bealal1995Dohal@gmail.com>
2021-11-01 10:13:22 +0300

    BelalDOHAL_Lab16_Segue

---
+++ BelalDOHAL_Lab16_Segue/BelalDOHAL_Lab16_Segue/ViewControllerThree.swift
@@ -0,0 +1,12 @@
+//
+//  ViewControllerThree.swift
+//  BelalDOHAL_Lab16_Segue
+//
+//  Created by Belal Dohal on 26/03/1443 AH.
+//
+
+import Foundation
+import UIKit
+class ViewControllerThree: UIViewController{
+    
+}
---
+++ BelalDOHAL_Lab16_Segue/BelalDOHAL_Lab16_Segue/ViewControllerTwo.swift
@@ -0,0 +1,12 @@
+//
+//  ViewControllerTwo.swift
+//  BelalDOHAL_Lab16_Segue
+//
+//  Created by Belal Dohal on 26/03/1443 AH.
+//
+
+import Foundation
+import UIKit
+class ViewControllerTwo: UIViewController{
+    
+}
