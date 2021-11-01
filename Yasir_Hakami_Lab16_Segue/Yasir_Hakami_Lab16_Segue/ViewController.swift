a4ab8233527303fd46aec32728a426120f3a5eb0
yasir <donyasser4@gmail.com>
2021-11-01 10:31:41 +0300

    YasirHakami

---
+++ Yasir_Hakami_Lab16_Segue/Yasir_Hakami_Lab16_Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yasir_Hakami_Lab16_Segue
+//
+//  Created by Yasir Hakami on 01/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Yasir_Hakami_Lab16_Segue/Yasir_Hakami_Lab16_Segue/ViewController3.swift
@@ -0,0 +1,13 @@
+//
+//  viewControll3.swift
+//  Yasir_Hakami_Lab16_Segue
+//
+//  Created by Yasir Hakami on 01/11/2021.
+//
+
+import Foundation
+import UIKit
+
+class ViewController3 : UIViewController {
+    
+}
